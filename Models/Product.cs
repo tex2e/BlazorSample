d1171af506e29d4b7f1f56d931549dc64ce6dd41
Mako <10919093+tex2e@users.noreply.github.com>
2024-10-05 21:35:47 +0900

    first commit

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+
+namespace BlazorSample.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public bool IsActive { get; set; } = true;
+    }
+}
