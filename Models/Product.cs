1bbfaa57c787177da5bc7bd23cffd32d957551a0
Mako <10919093+tex2e@users.noreply.github.com>
2024-10-26 12:52:47 +0900

    update

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,10 +1,28 @@
 
-namespace BlazorSample.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorSample.Models;
+
+public class Product
 {
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = "";
-        public bool IsActive { get; set; } = true;
-    }
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "名前は入力必須です")]
+    public string Name { get; set; } = "";
+
+    [Range(1, 1000, ErrorMessage = "値段は1円以上1000円以下を入力してください")]
+    public decimal Price { get; set; }
+
+    public bool IsActive { get; set; } = true;
+
+    public Category MyProperty { get; set; }
+
+    public DateOnly AvailableAfter { get; set; }
+}
+
+public enum Category
+{
+    Type1,
+    Type2,
+    Type3
 }
